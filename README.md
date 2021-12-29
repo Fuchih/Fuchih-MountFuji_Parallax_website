d91bddb75b0c295f7b5884f5a26403ae7cf52efd
FuChih <t19887348@hotmail.com>
2021-12-29 13:46:14 +0800

    update

--- README.md
+++
@@ -1,3 +0,0 @@
-# Fuchih-MountFuji_Parallax_website
-
-[DEMO](https://fuchih.github.io/Fuchih-MountFuji_Parallax_website/)
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-const sun = document.querySelector('#sun')
-const left = document.querySelector('#left')
-const right = document.querySelector('#right')
-const mount = document.querySelector('#mount')
-const temple = document.querySelector('#temple')
-
 window.addEventListener('scroll', () => {
-  let value = this.scrollY
+  const sun = document.querySelector('#sun')
+  const left = document.querySelector('#left')
+  const right = document.querySelector('#right')
+  const mount = document.querySelector('#mount')
+  const temple = document.querySelector('#temple')
+  const bridge = document.querySelector('#bridge')
+  const sakuraLeft = document.querySelector('#sakura-left')
+  const sakuraRight = document.querySelector('#sakura-right')
+  var value = this.scrollY
+
   if (value < 550) {
     sun.style.transform = `translate(-50%,${value}px)`
     left.style.transform = `translate(${value * 0.5}px, 0)`
     right.style.transform = `translate(-${value * 0.5}px, 0)`
     mount.style.transform = `translate(-50%, ${value * 0.5}px)`
     temple.style.transform = `translate(-${value * 0.3}px, 0)`
+    bridge.style.opacity = `${1 - value * 0.005}`
+    sakuraLeft.style.transform = `translate(-${value * 0.5}px, ${value * 1.1}px)`
+    sakuraRight.style.transform = `translate(${value * 0.5}px, ${value * 1.1}px)`
   }
+
+  if (value > 300) bridge.style.display = 'none'
+  else bridge.style.display = 'block'
 })
+
